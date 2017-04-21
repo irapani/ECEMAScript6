6490b9e462765b19bdb873836a6e4ac17114a151
irapani <uday.crazyuk@gmail.com>
2017-04-20 19:32:46 -0700

    Add files via upload

---
+++ es_compiled.js
@@ -0,0 +1,7 @@
+$traceurRuntime.registerModule("es.js", [], function() {
+  "use strict";
+  var __moduleName = "es.js";
+  console.log('Hello World');
+  return {};
+});
+$traceurRuntime.getModule("es.js" + '');
